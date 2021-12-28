714662e31485484199731c538f2d933859f90593
isaacmuchiri <isaacmuchiri98@gmail.com>
2021-12-28 19:36:25 +0300

    Added a new folder

---
+++ exceptions/test.py
@@ -0,0 +1,7 @@
+#prompting the user to enter the 2 numbers and also display the sum ov=f the two numbers
+def calculate_sum(a,b):
+    return a + b
+a= int(input('Enter value of a: '))
+b= int(input('Enter value of b: '))
+
+print('sum:', calculate_sum(a,b))
